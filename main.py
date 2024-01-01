6f59477b8d7b81d530c095e7c5056d06be38a566
Lev-Sh <65592917+lev-sh@users.noreply.github.com>
2024-01-01 15:48:37 +0300

    Start making game -main menu -apple box -collecting apples -kick away apples

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ COLOR = pygame.Color('yellow')
 FPS = 60
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
 def load_image(name: str, path: str = 'data', colorway: None | pygame.Color | str | int = None) -> pygame.Surface:
     fullname = os.path.join(path, name)
     if not os.path.isfile(fullname):
@@ -21,46 +26,9 @@ def load_image(name: str, path: str = 'data', colorway: None | pygame.Color | st
         if colorway == -1:
             colorway = image.get_at((0, 0))
         image.set_colorkey(colorway)
-    # else:
-    # image = image.convert_alpha()
+    else:
+        image = image.convert_alpha()
     return image
-
-
-tile_images = {
-    'wall': load_image('box.png', 'data/images'),
-    'empty': load_image('grass.png', 'data/images')
-}
-player_image = load_image('mar.png', 'data/images')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-        if tile_type == 'wall':
-            tiles_blocks_group.add(self)
-
-
-class Player(pygame.sprite.Sprite):
-    player_image = load_image('mar.png', 'data/images')
-
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.x = pos_x
-        self.y = pos_y
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-    def move(self, mx: int, my: int):
-        old_block = self.x, self.y
-        self.rect.move_ip(mx * tile_width, my * tile_height)
-        if pygame.sprite.spritecollideany(self, tiles_blocks_group):
-            self.rect.move_ip(-mx * tile_width, -my * tile_height)
 
 
 def load_level(filename, fullchar: str = '.') -> list[str]:
@@ -71,6 +39,16 @@ def load_level(filename, fullchar: str = '.') -> list[str]:
     return list(map(lambda x: x.ljust(max_width, fullchar), level_map))
 
 
+tile_images = {
+    'wall': load_image('box.png', 'data/images'),
+    'appbox': load_image('apple_box.png', 'data/images'),
+    'empty': load_image('floor.png', 'data/images')
+}
+player_image = load_image('player.png', 'data/images')
+apple_image = load_image('apple.png', 'data/images')
+tile_width = tile_height = 40
+
+
 def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
@@ -79,23 +57,23 @@ def generate_level(level):
                 Tile('empty', x, y)
             elif level[y][x] == '#':
                 Tile('wall', x, y)
+            elif level[y][x] == 'a':
+                Tile('appbox', x, y)
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
     return new_player, x + 1, y + 1
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
 def start_screen():
-    intro_text = ["НАЖМИТЕ ЛЮБУЮ КЛАВИШУ",
-                  "ЧТО БЫ НАЧАТЬ"]
+    intro_text = [""]
 
-    fon = pygame.transform.scale(load_image('fon.jpg', 'data/images'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('fon.png', 'data/images'), (WIDTH, HEIGHT))
+    start_btns = pygame.transform.scale(load_image('start_btn.png', 'data/images'), (80, 80))
+
     screen.blit(fon, (0, 0))
+    screen.blit(start_btns, (WIDTH / 2 - 80, HEIGHT / 2 - 80))
+
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
@@ -118,21 +96,44 @@ def start_screen():
         clock.tick(FPS)
 
 
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        if tile_type == 'wall':
+            tiles_blocks_group.add(self)
+        if tile_type == 'appbox':
+            apple_box_group.add(self)
+
+
+class Player(pygame.sprite.Sprite):
+    player_image = load_image('player.png', 'data/images')
+
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.inventory = "None"
+        self.x = pos_x
+        self.y = pos_y
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def move(self, mx: float, my: float):
+        self.image = player_image
+        old_block = self.x, self.y
+        self.rect.move_ip(mx * tile_width, my * tile_height)
+        if pygame.sprite.spritecollideany(self, tiles_blocks_group):
+            self.rect.move_ip(-mx * tile_width, -my * tile_height)
+
+    def get_item(self, item: str):
+        if pygame.sprite.spritecollideany(self, apple_box_group):
+            self.inventory = item
+
+
 
 if __name__ == '__main__':
-    camera = Camera()
-
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
     start_screen()
@@ -141,7 +142,7 @@ if __name__ == '__main__':
     tiles_group = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
     tiles_blocks_group = pygame.sprite.Group()
-
+    apple_box_group = pygame.sprite.Group()
     running = True
     started = False
 
@@ -149,28 +150,31 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and not started:
                 screen.fill(BACKGROUND)
                 pl, xs, ys = generate_level(load_level('level1.dat'))
                 started = True
-            elif event.type == pygame.KEYDOWN and started:
-                match event.key:
-                    case pygame.K_w:
-                        pl.move(0, -1)
-                    case pygame.K_s:
-                        pl.move(0, 1)
-                    case pygame.K_a:
-                        pl.move(-1, 0)
-                    case pygame.K_d:
-                        pl.move(1, 0)
-
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
+                pl.get_item("apple")
+                print(pl.inventory)
+            if keys[pygame.K_q]:
+                pl.get_item("None")
+                print(pl.inventory)
+            if keys[pygame.K_w]:
+                player_image = load_image('player.png', 'data/images')
+                pl.move(0, -0.1)
+            elif keys[pygame.K_s]:
+                player_image = load_image('player_180.png', 'data/images')
+                pl.move(0, 0.1)
+            elif keys[pygame.K_a]:
+                player_image = load_image('player_270.png', 'data/images')
+                pl.move(-0.1, 0)
+            elif keys[pygame.K_d]:
+                player_image = load_image('player_90.png', 'data/images')
+                pl.move(0.1, 0)
         tiles_group.draw(screen)
         player_group.draw(screen)
-        if started:
-            camera.update(pl)
-            # обновляем положение всех спрайтов
-            for sprite in all_sprites:
-                camera.apply(sprite)
         pygame.display.flip()
         clock.tick(FPS)
-    pygame.quit()
+    terminate()
